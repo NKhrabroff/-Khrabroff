eef7e1babec95df228a15ab9bec481abd56e4b05
NKhrabroff <79683082+NKhrabroff@users.noreply.github.com>
2021-03-21 04:43:18 +0300

    Add files via upload

---
+++ dz_1_2.py
@@ -0,0 +1,32 @@
+<<<<<<< Updated upstream
+def sum_num(value):
+    res = 0
+
+    while value != 0:
+        res += value % 10
+        value //= 10
+
+    return res
+arr = [i**3 for i in range(1, 1001, 2)]
+
+res_1 = sum(filter(lambda num: sum_num(num) % 7 == 0, arr))
+res_2 = sum(filter(lambda num: sum_num(num + 17) % 7 == 0, arr))
+
+print(res_1)
+=======
+def sum_num(value):
+    res = 0
+
+    while value != 0:
+        res += value % 10
+        value //= 10
+
+    return res
+arr = [i**3 for i in range(1, 1001, 2)]
+
+res_1 = sum(filter(lambda num: sum_num(num) % 7 == 0, arr))
+res_2 = sum(filter(lambda num: sum_num(num + 17) % 7 == 0, arr))
+
+print(res_1)
+>>>>>>> Stashed changes
+print(res_2)
